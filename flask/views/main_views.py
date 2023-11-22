8b6e1b162d6b15bd8561b2a89ee897f807730ab8
yeongseo <ssucsh1@gmail.com>
2023-11-23 00:07:40 +0900

    Dgdg

--- flask/views/main_views.py
+++ flask/views/main_views.py
@@ -12,34 +12,34 @@ from flask_socketio import SocketIO, emit
 #import pymysql
 
 
-bp = Blueprint('main', __name__, template_folder="templates")
+# bp = Blueprint('main', __name__, template_folder="templates")
 
 
-mp_drawing = mp.solutions.drawing_utils # Visualizing our poses
-mp_pose = mp.solutions.pose # Importing our pose estimation model (ex)hand,
+# mp_drawing = mp.solutions.drawing_utils # Visualizing our poses
+# mp_pose = mp.solutions.pose # Importing our pose estimation model (ex)hand,
 
 
-@bp.route('/')
-def home():
-    return render_template('home.html')
+# @bp.route('/')
+# def home():
+#     return render_template('home.html')
 
-@bp.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
+# @bp.route('/calendar')
+# def calendar():
+#     return render_template('calendar.html')
 
-@bp.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@bp.route('/register')
-def register():
-    return render_template('register.html')
+# @bp.route('/login')
+# def login():
+#     return render_template('login.html')
 
 
-@bp.route('/contact')
-def contact():
-    return render_template('contact.html')
+# @bp.route('/register')
+# def register():
+#     return render_template('register.html')
+
+
+# @bp.route('/contact')
+# def contact():
+#     return render_template('contact.html')
 
 
 
