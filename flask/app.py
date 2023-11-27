5aa4b584ce3c9f44bf4d33b7928683a1308fd5a8
soyeon <thdus6457@naver.com>
2023-11-28 08:29:41 +0900

    캘린더db연결

--- flask/app.py
+++ flask/app.py
@@ -43,10 +43,76 @@ app.register_blueprint(log,url_prefix='/')
 def home():
     return render_template('home.html')
 
+# 이벤트 추가(캘린더)
+@app.route('/add_event', methods=['POST'])
+def add_event():
+    if not session.get('user_id'):
+        return jsonify({'status': 'error', 'message': 'Not logged in'}), 401
+
+    try:
+        # 요청으로부터 데이터 추출
+        data = request.get_json()
+        user_id = session.get('user_id')
+        date = data.get('date')
+        memo = data.get('memo')
+
+        # 문자열을 날짜 객체로 변환
+        date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+        # 데이터베이스에 쿼리 실행
+        cursor = db.cursor()
+        query = "INSERT INTO calendar (user_id, date, memo) VALUES (%s, %s, %s)"
+        cursor.execute(query, (user_id, date_obj, memo))
+        db.commit()
+        cursor.close()
+
+        return jsonify({'status': 'success'})
+    except Exception as e:
+        # 예외 발생 시 오류 메시지 반환
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
 # 기록 페이지
 @app.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
+def calendar_page():
+    try:
+        cursor = db.cursor()
+        query = "SELECT id, date, memo FROM calendar"
+        cursor.execute(query)
+        events = cursor.fetchall()
+        cursor.close()
+        
+        # Format events for JavaScript
+        formatted_events = [{
+            'id': event[0],
+            'date': event[1].strftime('%Y-%m-%d'),
+            'memo': event[2]
+        } for event in events]
+
+        return render_template('calendar.html', events=formatted_events)
+    except Exception as e:
+        print(e)
+        return render_template('calendar.html', events=[])
+
+@app.route('/delete_event', methods=['POST'])
+def delete_event():
+    if not session.get('user_id'):
+        return jsonify({'status': 'error', 'message': 'Not logged in'}), 401
+
+    try:
+        data = request.get_json()
+        date = data.get('date')
+        memo = data.get('memo')
+
+
+        cursor = db.cursor()
+        query = "DELETE FROM calendar WHERE date = %s AND memo = %s"
+        cursor.execute(query, (date, memo))
+        db.commit()
+        cursor.close()
+
+        return jsonify({'status': 'success'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 @app.route('/static/video/warrior2.mp4')
 def get_video():
