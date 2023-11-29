4404088a8b12528794d2b228bf5e7273fcab51c3
Git-panic <bomsu3436@gmail.com>
2023-11-29 11:04:31 +0900

    운동페이지

--- flask/app.py
+++ flask/app.py
@@ -164,9 +164,10 @@ def instructions():
     return render_template('instructions.html')
 
 # 운동 페이지
-@app.route('/timer')
-def timer():
-    return render_template('timer.html')
+@app.route('/exercise')
+def exercise():
+    return render_template('exercise.html')
+
 
 
 
